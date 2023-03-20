eb5807761d2d0db36ab92f18878f1260d9021147
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-03-20 17:02:14 +0100

    project4: network: new_post: it works, save to base, started with all post presentation

--- network/models.py
+++ network/models.py
@@ -1,6 +1,18 @@
+"""
+ add additional models to this file to represent details about posts, likes, and followers.
+"""
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    body = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} posted {self.body} at {self.timestamp}"
--- network/views.py
+++ network/views.py
@@ -1,14 +1,44 @@
+"""
+Views for the network app.
+New Post: Users who are signed in should be able to write a new text-based post by filling in text into a text area and then clicking a button to submit the post.
+The screenshot at the top of this specification shows the “New Post” box at the top of the “All Posts” page. You may choose to do this as well, or you may make the “New Post” feature a separate page.
+Posts should be displayed, most recent first.
+"""
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from .models import User, Post
+from django.contrib.auth.decorators import login_required
 
 from .models import User
 
 
+class NewPostForm(forms.Form):
+    body = forms.CharField(widget=forms.Textarea, label="")
+
+
 def index(request):
-    return render(request, "network/index.html")
+    return render(request, "network/index.html",
+                  {"new_post_form": NewPostForm(),
+                   "posts": Post.objects.all().order_by("-timestamp"),
+                  })
+
+
+@login_required(login_url='/login')
+def new_post(request):
+    user_by_id = User.objects.get(id=request.user.id)
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            body = form.cleaned_data["body"]
+            post = Post(body=body, user=user_by_id)
+            post.save()
+    # redirect to index
+    return HttpResponseRedirect(reverse("index"))
 
 
 def login_view(request):
