cff2115092ec62c421b640872dd0bf40c5a64752
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-03-22 17:13:05 +0100

    project4: network: profile_page: small changes on profile page

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -2,7 +2,7 @@
 // if true increase the number of followers else decrease the number of followers
 
 document.addEventListener('DOMContentLoaded', function() {
-    // get the user id from the url and save it to a variable
+    // third child of the follow-form is the follow or unfollow button
     document.querySelector('#follow-form input:nth-child(3)').addEventListener('click', () => {
         // add event log to print the event to console
         console.log('Follow button was clicked');
