656cc0688df2d571f2da648a152bd2cf82e58003
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-03-24 14:45:34 +0100

    project4: network: edit_post: I got an log in console that button is clicked

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,8 +1,23 @@
 // if edit button clicked then:
 // add console log
 // hide #post-body
-
-const editButton = document.querySelector(#edit-button);
-editButton.addEventListener('click', function() {
-  // Your code to handle the "Edit" button click goes here
+console.log('JavaScript code is actually being executed')
+document.addEventListener('DOMContentLoaded', function() {
+    let editButton = document.querySelector('#edit-button-2');
+    if (editButton) {
+        editButton.addEventListener('click', function (event) {
+            console.log('edit button clicked');
+        });
+    }
 });
+
+// add function that add event listener to edit-button-2 in fuser is logged in  add log to console that edit-button-2 clicked
+
+function edit_button_clicked() {
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector('#edit-button-2').addEventListener('click', function (event) {
+            console.log('edit button clicked');
+        });
+    });
+  console.log('edit button clicked');
+}
